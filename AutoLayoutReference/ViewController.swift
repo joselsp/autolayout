103a380a4f3d1313147921485ac40acdbc67009c
memphis <memphis@memphiss-iMac.local>
2017-07-30 09:11:27 +0200

    Initial Commit

---
+++ AutoLayoutReference/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutReference
+//
+//  Created by memphis on 30/07/2017.
+//  Copyright © 2017 keepcoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
